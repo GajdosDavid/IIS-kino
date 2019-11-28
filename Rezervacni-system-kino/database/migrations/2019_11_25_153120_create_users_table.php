d5968a6e2174d7376a5d7987f42fdb46e4f8c207
Vennny <toboqan@seznam.cz>
2019-11-28 15:33:23 +0100

    name a middleName -> firstName

--- Rezervacni-system-kino/database/migrations/2019_11_25_153120_create_users_table.php
+++ Rezervacni-system-kino/database/migrations/2019_11_25_153120_create_users_table.php
@@ -15,8 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('middleName')->nullable();
+            $table->string('firstName');
             $table->string('surname');
             $table->string('phone');
             $table->string('mail');
