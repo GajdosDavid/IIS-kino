c7f77600be12d9f385d34ae146a41b81f20fb286
Vennny <toboqan@seznam.cz>
2019-11-30 22:42:59 +0100

    Kapacita gone

--- Rezervacni-system-kino/resources/views/hall/show.blade.php
+++ Rezervacni-system-kino/resources/views/hall/show.blade.php
@@ -6,7 +6,7 @@
 @section('content')
     <h1>{{ $hall->name }}</h1>
     adresa: {!! $hall->address !!}<br>
-    kapacita: {!! $hall->capacity !!}<br>
+    kapacita: {!! $hall->rows * $hall->seats_in_row !!}<br>
     řad: {!! $hall->rows !!}<br>
     sedadel v řadě: {!! $hall->seats_in_row !!}
 @endsection
