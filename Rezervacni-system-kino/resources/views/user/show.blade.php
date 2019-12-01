0865cdf50d3db1bf978777f0f153ce777f58bb7e
Vennny <toboqan@seznam.cz>
2019-12-01 19:05:15 +0100

    Bugfix, session lifetime na hodinu a vypsani data vytvoreni u user detail

--- Rezervacni-system-kino/resources/views/user/show.blade.php
+++ Rezervacni-system-kino/resources/views/user/show.blade.php
@@ -7,11 +7,12 @@
     @guest
         <h1>K této stránce nemáte přístup</h1>
     @else
-        @if (Auth::user()->id == $reservation->user->id || Auth::user()->role == 3 || Auth::user()->role == 1)
+        @if (Auth::user()->id == $user->id || Auth::user()->role == 3 || Auth::user()->role == 1)
             <h1>{{ $user->first_name.' '.$user->surname}}</h1>
             <p>email: {!! $user->email !!}</p>
             <p>phone: {!! $user->phone !!}</p>
             <p>role: {!! $user->role !!}</p>
+            <p>vytvořen: {!! $user->created_at !!}</p>
         @else
             <h1>K této stránce nemáte přístup</h1>
         @endif
