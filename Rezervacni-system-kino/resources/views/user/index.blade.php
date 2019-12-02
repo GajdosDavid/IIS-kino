534a5c9da8aa9c44b0d8e725e67ca090f767ae5a
Vennny <toboqan@seznam.cz>
2019-12-03 00:11:57 +0100

    script u usera

--- Rezervacni-system-kino/resources/views/user/index.blade.php
+++ Rezervacni-system-kino/resources/views/user/index.blade.php
@@ -68,3 +68,32 @@
         @endif
     @endguest
 @endsection
+
+@push('scripts')
+    <script type="application/javascript">
+        function searchTable() {
+
+            let input, filter, table, tr, td, i, j, cell;
+            input = document.getElementById("searchReservations");
+            filter = input.value.toUpperCase();
+            table = document.getElementById("reservations");
+            tr = table.getElementsByTagName("tr");
+
+            for (i = 1; i < tr.length; i++) {
+
+                tr[i].style.display = "none";
+
+                td = tr[i].getElementsByTagName("td");
+                for (j = 0; j < td.length -1; j++) {
+                    cell = tr[i].getElementsByTagName("td")[j];
+                    if (cell) {
+                        if (cell.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                            tr[i].style.display = "";
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    </script>
+@endpush
